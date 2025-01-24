7c6f51f82b1c3d1314ea21391c88bb3b4f3c7af3
Eva Hamrud <50098063+evaham1@users.noreply.github.com>
2025-01-24 15:36:05 +1100

    extract LUPINE_single outside of wrapper and check reproducibility

--- examine_LUPINE_behaviour.R
+++ examine_LUPINE_behaviour.R
@@ -8,7 +8,7 @@ data("HFHSdata")
 debug(LUPINE)
 debug(LUPINE_single)
 
-# try what is the difference for is.transformed TRUE/FALSE
+# run LUPINE_single through LUPINE wrapper
 net_Normal <- LUPINE(HFHSdata$OTUdata_Normal,
                      is.transformed = FALSE,
                      lib_size = HFHSdata$Lib_Normal,
@@ -17,38 +17,54 @@ net_Normal <- LUPINE(HFHSdata$OTUdata_Normal,
                      cutoff = 0.05
 )
 
-# quick visualisation to check for consistency
-Day0 <- HFHSdata$OTUdata_Normal[, , 1]
-taxa_info <- HFHSdata$filtered_taxonomy[colnames(Day0), ]
-taxa_info$X5 <- factor(taxa_info$X5)
-col_vec <- rep(
-  c(
-    "green", "gray", "darkgreen", "darkred", "firebrick2", "pink",
-    "tomato", "orange", "blue", "purple", "hotpink", "lightblue"
-  ),
-  summary(taxa_info$X5)
-)
-p1 <- netPlot_HFHS(net_Normal[[3]], col_vec, "Normal Day7")
-p1
+# check outputs
+length(net_Normal) # 4, each timepoints
+dim(net_Normal[[1]]) # 212 x 212 variable interaction table
+table(net_Normal[[1]])
+# 0     1
+# 43084  1860
 
-net_Normal <- LUPINE(HFHSdata$OTUdata_Normal,
-                     is.transformed = FALSE,
-                     lib_size = HFHSdata$Lib_Normal,
-                     ncomp = 1, single = TRUE,
-                     excluded_taxa = HFHSdata$low_Normal_taxa,
-                     cutoff = 0.05
-)
+# run LUPINE_single outside LUPINE wrapper
+cutoff = 0.05
+res <- sapply(1:4, function(d) {
+  net <- LUPINE_single(HFHSdata$OTUdata_Normal,
+                       timepoint = d,
+                       excluded_taxa = HFHSdata$low_Normal_taxa,
+                       is.transformed = FALSE,
+                       lib_size = HFHSdata$Lib_Normal,
+                       ncomp = 1
+  )$pvalue
+  net <- apply(net<cutoff, c(1, 2), function(x) {
+    ifelse(is.na(x), 0, x)
+  })
+  return(net)
+}, simplify = FALSE)
 
-# quick visualisation to check for consistency
-Day0 <- HFHSdata$OTUdata_Normal[, , 1]
-taxa_info <- HFHSdata$filtered_taxonomy[colnames(Day0), ]
-taxa_info$X5 <- factor(taxa_info$X5)
-col_vec <- rep(
-  c(
-    "green", "gray", "darkgreen", "darkred", "firebrick2", "pink",
-    "tomato", "orange", "blue", "purple", "hotpink", "lightblue"
-  ),
-  summary(taxa_info$X5)
-)
-p1 <- netPlot_HFHS(net_Normal[[3]], col_vec, "Normal Day7")
-p1
+# check outputs
+length(res)
+dim(res[[1]])
+table(res[[1]])
+# all same as above
+
+# run LUPINE_single_timepoint in exact same way
+cutoff = 0.05
+res <- sapply(1:4, function(d) {
+  net <- LUPINE_single_timepoint(HFHSdata$OTUdata_Normal,
+                       timepoint = d,
+                       excluded_taxa = HFHSdata$low_Normal_taxa,
+                       is.transformed = FALSE,
+                       lib_size = HFHSdata$Lib_Normal,
+                       ncomp = 1
+  )$pvalue
+  net <- apply(net<cutoff, c(1, 2), function(x) {
+    ifelse(is.na(x), 0, x)
+  })
+  return(net)
+}, simplify = FALSE)
+
+
+
+
+
+
+
