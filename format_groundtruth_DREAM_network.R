7d2dce622d40f0b0e164d528b35af52b034d309d
Eva Hamrud <50098063+evaham1@users.noreply.github.com>
2025-01-28 11:41:51 +1100

    format ground truth DREAM4 ntwrk and make it undirected

--- format_groundtruth_DREAM_network.R
+++ format_groundtruth_DREAM_network.R
@@ -2,21 +2,42 @@
 
 library(tidyverse)
 
-# read in gold-standard network data, boolean to indicate interaction or not
+## read in gold-standard network data, table with one gene in each column and then boolean to indicate interaction or not
 network_1_data <- read.csv("./data/DREAM4/insilico_size100_1_goldstandard.tsv", sep = "\t")
-colnames(network_1_data) <- c("Var1", "Var2", "Value") # 9899 rows - does not include every possible interaction
+colnames(network_1_data) <- c("Var1", "Var2", "Value")
 
-# filter to only include rows displaying an interaction (i.e. Value = 1)
-network_1_data <- network_1_data %>% filter(Value == 1) # only keep rows which have an interaction, 175 rows left
+head(network_1_data)
+# Var1 Var2 Value
+# 1   G1   G3     1
+# 2   G1   G4     1
 
-# filter to remove duplicate rows (order of Var1 and Var2 doesn't matter)
-network_1_data[c("Var1", "Var2")] <- t(apply(network_1_data[c("Var1", "Var2")], 1, sort))
-duplicates <- network_1_data[duplicated(network_1_data[c("Var1", "Var2")]) | duplicated(network_1_data[c("Var1", "Var2")], fromLast = TRUE), ]
-nrow(duplicates) # 14 duplicates i.e. 7 times 2
-network_1_data <- network_1_data %>%
-  filter(!(Var1 %in% duplicates$Var1 & Var2 %in% duplicates$Var2) |
-           (duplicated(network_1_data[c("Var1", "Var2")]) == FALSE))
-nrow(network_1_data) # 168 true interactions left in data
+# how many variables and therefore possible pairwise interactions are there
+length(unique(c(network_1_data$Var1, network_1_data$Var2))) # 100 unique variables
+# for 100 variables, the total number of pairwise interactions (undirected) possible excluding self-interactions is:
+(100*99) / 2 # 4950
+nrow(network_1_data) # 9899 - this is about double, because this network is DIRECTED
+
+## need to turn the directed network into an undirected one
+# i.e. if A interacts with B, there is an interaction between A and B
+
+# add interaction names ordered so that we can identify duplicates
+network_1_data_sorted <- network_1_data %>%
+  mutate(Pair = ifelse(Var1 < Var2, paste(Var1, Var2, sep = "_"), paste(Var2, Var1, sep = "_")))
+length(unique(network_1_data_sorted$Pair)) # 4950 - now we have covered all pairwise undirected interactions
+
+# aggregate by Pair and set 'Value' to 1 if there is any interaction in either direction
+network_undirected <- network_1_data_sorted %>%
+  group_by(Pair) %>%
+  summarize(Value = ifelse(any(Value == 1), 1, 0), .groups = "drop") %>%
+  separate(Pair, into = c("Var1", "Var2"), sep = "_")
+
+# checks
+nrow(network_undirected) # 4950 pairwise interactions covered
+table(network_undirected$Value)
+# 0    1
+# 4782  168
+
+## Change format from dataframe into a symmetrical matrix with variables on rows and cols in order
 
 # ## making test data to transform to a matrix
 # target_mat <- matrix(c(0, 0, 1, 1, 0,
@@ -36,7 +57,7 @@ nrow(network_1_data) # 168 true interactions left in data
 # Convert tsv of network from DREAM4 into matrix like LUPINE output
 variables <- paste0("G", seq(1, 100))
 network_1 <- matrix(0, nrow = 100, ncol = 100)
-data <- network_1_data
+data <- network_undirected
 
 diag(network_1) <- NA
 rownames(network_1) <- variables
@@ -57,7 +78,10 @@ network_1
 
 # check total number of interactions is the same
 sum(network_1 == 1, na.rm = TRUE) / 2 # 168
-sum(data$Value) # 175
+sum(data$Value) # 168
+
+# check matrix is symmetrical
+all(is.na(network_1) | network_1 == t(network_1)) # TRUE
 
 # save reformatted network
 saveRDS(network_1, "./data/DREAM4/DREAM4_network_1_goldstandard.RDS")
