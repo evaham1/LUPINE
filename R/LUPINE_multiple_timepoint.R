538ab19bbbff8b2428fa5e861a6916f9c06308ad
Eva Hamrud <50098063+evaham1@users.noreply.github.com>
2025-02-26 12:21:16 +1100

    run in parallel

--- R/LUPINE_multiple_timepoint.R
+++ R/LUPINE_multiple_timepoint.R
@@ -2,17 +2,18 @@
 #'
 #' @param data A 3D array of counts or transformed data with dimensions samples x taxa x time points. The last timepoint in this data will be considered the 'current' one
 #' for which the correlations are calculated
-#' @param is.transformed A logical indicating whether the data is transformed or not
-#' @param lib_size A matrix of library sizes for each sample and time point
+#' @param lib_size A matrix of library sizes for each sample and time point, optional.
+#' If used, refression models will be run on log-transformed data with library size as an offset.
 #' @param ncomp The number of components to use for dimensionality reduction
+#' @param BPPARAM A \linkS4class{BiocParallelParam} object indicating the type of parallelisation
 #'
 #' @return A list of correlations and p-values
 #' @export
 #'
 LUPINE_multiple_timepoint <- function(data,
-                                      is.transformed = FALSE,
                                       lib_size = NULL,
-                                      ncomp = 1) {
+                                      ncomp = 1,
+                                      BPPARAM = SerialParam()) {
 
   # Extract the data from the final timepoint
   data_final_timepoint <- data[, , dim(data)[3]] # equivalent to count_day <- data[, , day]
@@ -23,8 +24,8 @@ LUPINE_multiple_timepoint <- function(data,
 
   # Generate all pairwise combinations of variables for correlation analysis
   len <- nVar * (nVar - 1) / 2
-  taxa1 <- unlist(lapply(1:nVar, function(i) rep(i, (nVar - i))))
-  taxa2 <- unlist(lapply(2:nVar, function(i) seq(i, nVar, 1)))
+  var1 <- unlist(lapply(1:nVar, function(i) rep(i, (nVar - i))))
+  var2 <- unlist(lapply(2:nVar, function(i) seq(i, nVar, 1)))
 
   # Initialize matrices to store partial correlation coefficients and p-values
   pcor <- matrix(NA, nrow = nVar, ncol = nVar)
@@ -35,52 +36,54 @@ LUPINE_multiple_timepoint <- function(data,
   # Perform dimensionality reduction
   loadings_m <- PLS_approx(data, dim(data)[3], ncomp = ncomp, taxa_names = var_names)
 
-  if (is.null(lib_size) & !is.transformed) {
-    # Creating library size by summing taxa counts per sample and time point
-    lib_size <- apply(data, c(1, 3), sum)
-  }
-
-  # Extract count array after excluding taxa for current day
-  # data_day_f <- data[, var_names, dim(data)[3]]
-
-
-  for (i in 1:len) {
-    # print(i)
+  # Define a function to compute partial correlations for a single pair
+  compute_partial_corr <- function(i) {
     loading_tmp <- loadings_m
-    loading_tmp[c(taxa1[i], taxa2[i]), ] <- 0
-    u1 <- scale(data_final_timepoint, center = TRUE, scale = TRUE) %*% loading_tmp
-    if (!is.transformed) {
-      ##** LUPINE with counts**##
-      # principal component regression on log counts+1 with an offset for library size
-      r_i <- lm(log(data_final_timepoint[, taxa1[i]] + 1) ~ u1, offset = log(lib_size[, dim(data)[3]]))
-      r_j <- lm(log(data_final_timepoint[, taxa2[i]] + 1) ~ u1, offset = log(lib_size[, dim(data)[3]]))
+    loading_tmp[c(var1[i], var2[i]), ] <- 0 # Remove loading vectors of the current variable pair
+    u1 <- scale(data_final_timepoint, center = TRUE, scale = TRUE) %*% loading_tmp # Dimension reduced data
+
+    # Fit linear models for both variables - may account for library size / log normalise
+    r_i <- if (is.null(lib_size)) {
+      lm(data_final_timepoint[, var1[i]] ~ u1)
     } else {
-      ##** LUPINE with clr**##
-      # principal component regression on clr
-      r_i <- lm(data_final_timepoint[, taxa1[i]] ~ u1)
-      r_j <- lm(data_final_timepoint[, taxa2[i]] ~ u1)
+      lm(log(data_final_timepoint[, var1[i]] + 1) ~ u1, offset = log(lib_size[, dim(data)[3]]))
     }
-    # partial correlation calculation
-    pcor[taxa1[i], taxa2[i]] <- pcor[taxa2[i], taxa1[i]] <- cor.test(r_i$residuals,
-                                                                     r_j$residuals,
-                                                                     method = "pearson"
-    )$estimate
-    pcor.pval[taxa1[i], taxa2[i]] <- pcor.pval[taxa2[i], taxa1[i]] <- cor.test(r_i$residuals,
-                                                                               r_j$residuals,
-                                                                               method = "pearson"
-    )$p.value
+    r_j <- if (is.null(lib_size)) {
+      lm(data_final_timepoint[, var2[i]] ~ u1)
+    } else {
+      lm(log(data_final_timepoint[, var2[i]] + 1) ~ u1, offset = log(lib_size[, dim(data)[3]]))
+    }
+
+    # Calculate partial correlation and p-value
+    list(
+      var1 = var1[i],
+      var2 = var2[i],
+      pcor_estimate = cor.test(r_i$residuals, r_j$residuals, method = "pearson")$estimate,
+      pcor_pval = cor.test(r_i$residuals, r_j$residuals, method = "pearson")$p.value
+    )
   }
 
+  # Apply the function in parallel to all variable pairs
+  pcor_list <- bplapply(1:len, compute_partial_corr, BPPARAM = BPPARAM)
+
+  # Populate the partial correlation and p-value matrices
+  for (result in pcor_list) {
+    pcor[result$var1, result$var2] <- pcor[result$var2, result$var1] <- result$pcor_estimate
+    pcor.pval[result$var1, result$var2] <- pcor.pval[result$var2, result$var1] <- result$pcor_pval
+  }
+
+  # Create full matrices for partial correlations and p-values
   pcor.full <- matrix(NA, nrow = nVar, ncol = nVar)
   pcor.pval.full <- matrix(NA, nrow = nVar, ncol = nVar)
   colnames(pcor.full) <- colnames(pcor.pval.full) <- colnames(data)
   rownames(pcor.full) <- rownames(pcor.pval.full) <- colnames(data)
 
+  # Populate the full matrices with the calculated partial correlations
   common_rows_cols <- intersect(rownames(pcor.full), rownames(pcor))
-
   pcor.full[common_rows_cols, common_rows_cols] <- pcor[common_rows_cols, common_rows_cols]
   pcor.pval.full[common_rows_cols, common_rows_cols] <- pcor.pval[common_rows_cols, common_rows_cols]
 
+  # Return the results as a list
   res <- list(Estimate = pcor.full, pvalue = pcor.pval.full)
 
   return(res)
