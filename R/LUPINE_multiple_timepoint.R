e133f926dd9c6d6f0a6461dce7cd0bc033713bb0
Eva Hamrud <50098063+evaham1@users.noreply.github.com>
2025-02-26 11:51:08 +1100

    remove excluded_taxa arg from LUPINE_multiple_timepoint

--- R/LUPINE_multiple_timepoint.R
+++ R/LUPINE_multiple_timepoint.R
@@ -2,7 +2,6 @@
 #'
 #' @param data A 3D array of counts or transformed data with dimensions samples x taxa x time points. The last timepoint in this data will be considered the 'current' one
 #' for which the correlations are calculated
-#' @param excluded_taxa A list of taxa to be excluded at each time point
 #' @param is.transformed A logical indicating whether the data is transformed or not
 #' @param lib_size A matrix of library sizes for each sample and time point
 #' @param ncomp The number of components to use for dimensionality reduction
@@ -11,7 +10,6 @@
 #' @export
 #'
 LUPINE_multiple_timepoint <- function(data,
-                                      excluded_taxa = NULL,
                                       is.transformed = FALSE,
                                       lib_size = NULL,
                                       ncomp = 1) {
@@ -19,13 +17,12 @@ LUPINE_multiple_timepoint <- function(data,
   # Extract the data from the final timepoint
   data_final_timepoint <- data[, , dim(data)[3]] # equivalent to count_day <- data[, , day]
 
-  # Total number of taxa (p)
-  nOTU_total <- dim(data)[2]
+  # Extract variable names and calculate the number of variables
+  var_names <- colnames(data_final_timepoint)
+  nVar <- dim(data)[2]
 
-  # Extract taxa names after excluded taxa
-  taxa_names <- colnames(data_final_timepoint)[!(colnames(data_final_timepoint) %in% excluded_taxa[[dim(data)[3]]])]
   # pairwise taxa combinations
-  nOTU <- length(taxa_names)
+  nOTU <- length(var_names)
   len <- nOTU * (nOTU - 1) / 2
   taxa1 <- unlist(lapply(1:nOTU, function(i) rep(i, (nOTU - i))))
   taxa2 <- unlist(lapply(2:nOTU, function(i) seq(i, nOTU, 1)))
@@ -33,17 +30,17 @@ LUPINE_multiple_timepoint <- function(data,
   # Initialisation
   pcor <- matrix(NA, nrow = nOTU, ncol = nOTU)
   pcor.pval <- matrix(NA, nrow = nOTU, ncol = nOTU)
-  colnames(pcor) <- colnames(pcor.pval) <- taxa_names
-  rownames(pcor) <- rownames(pcor.pval) <- taxa_names
+  colnames(pcor) <- colnames(pcor.pval) <- var_names
+  rownames(pcor) <- rownames(pcor.pval) <- var_names
 
   if (is.null(lib_size) & !is.transformed) {
     # Creating library size by summing taxa counts per sample and time point
     lib_size <- apply(data, c(1, 3), sum)
   }
   # Extract count array after excluding taxa for current day
-  data_day_f <- data[, taxa_names, dim(data)[3]]
+  data_day_f <- data[, var_names, dim(data)[3]]
 
-  loadings_m <- PLS_approx(data, dim(data)[3], ncomp = ncomp, taxa_names = taxa_names)
+  loadings_m <- PLS_approx(data, dim(data)[3], ncomp = ncomp, taxa_names = var_names)
 
 
   for (i in 1:len) {
@@ -73,8 +70,8 @@ LUPINE_multiple_timepoint <- function(data,
     )$p.value
   }
 
-  pcor.full <- matrix(NA, nrow = nOTU_total, ncol = nOTU_total)
-  pcor.pval.full <- matrix(NA, nrow = nOTU_total, ncol = nOTU_total)
+  pcor.full <- matrix(NA, nrow = nVar, ncol = nVar)
+  pcor.pval.full <- matrix(NA, nrow = nVar, ncol = nVar)
   colnames(pcor.full) <- colnames(pcor.pval.full) <- colnames(data)
   rownames(pcor.full) <- rownames(pcor.pval.full) <- colnames(data)
 
